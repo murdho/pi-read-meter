58462ded18576ca3ac192afd4cc6fcac20362ad3
Murdho <murdho@murdho.com>
2018-09-08 22:32:57 +0300

    Show command output for simpler debugging

--- camera.go
+++ camera.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/pkg/errors"
 )
@@ -28,8 +30,13 @@ type Camera struct {
 
 // Capture takes a pic to the specified file location.
 func (c *Camera) Capture(filePath string) error {
-	argsWithFile := fmt.Sprintf(c.captureArgs, filePath)
-	if err := exec.Command(c.captureCommand, argsWithFile).Run(); err != nil {
+	argsWithFile := strings.Split(fmt.Sprintf(c.captureArgs, filePath), " ")
+
+	cmd := exec.Command(c.captureCommand, argsWithFile...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
 		return errors.Wrap(err, "executing capture command failed")
 	}
 
